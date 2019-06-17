f4cea86bec4d421e79df9f3dc43b9f084a1ee6ab
Noldish <noldish@gmail.com>
2019-06-17 16:27:20 +0400

    missed cdi bean

--- src/main/java/com/discordee/web/WeatherRequestRepository.java
+++ src/main/java/com/discordee/web/WeatherRequestRepository.java
@@ -2,10 +2,12 @@ package com.discordee.web;
 
 import com.discordee.entity.WeatherRequest;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
+@ApplicationScoped
 public class WeatherRequestRepository {
 
     @PersistenceContext(unitName = "standalonePu")
