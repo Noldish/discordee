cf5198d4d97091cfdc2b954c8bed1ec878b9d3da
Noldish <noldish@gmail.com>
2019-10-15 09:46:53 +0400

    healthcheck message fix

--- src/main/java/com/discordee/web/HealthCheckService.java
+++ src/main/java/com/discordee/web/HealthCheckService.java
@@ -9,12 +9,9 @@ import javax.enterprise.context.ApplicationScoped;
 @Health
 @ApplicationScoped
 public class HealthCheckService implements HealthCheck {
-    @Override
+
     public HealthCheckResponse call() {
-        return HealthCheckResponse.named("health")
-                .up()
-                .withData("Author", "Ivo Woltring")
-                .withData("Website", "https://www.ivonet.nl")
-                .build();
+        return HealthCheckResponse.named("healthCheck").withData("foo", "bar").build();
+
     }
 }
