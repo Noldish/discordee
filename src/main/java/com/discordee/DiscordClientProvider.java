e3ccc77006237009fd954402b79b1b42a8d51a0d
Noldish <noldish@gmail.com>
2020-06-25 18:03:25 +0400

    fix quarkus healthcheck

--- src/main/java/com/discordee/DiscordClientProvider.java
+++ src/main/java/com/discordee/DiscordClientProvider.java
@@ -5,11 +5,13 @@ import discord4j.core.DiscordClient;
 import discord4j.core.DiscordClientBuilder;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Initialized;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+import java.util.Base64;
 
 @ApplicationScoped
 public class DiscordClientProvider {
@@ -17,7 +19,15 @@ public class DiscordClientProvider {
     private DiscordClient client;
 
     @Inject @ConfigProperty(name = "bot.token")
+    String encodedForecastKey;
+
     private String token;
+
+    @PostConstruct
+    private void init(){
+        byte[] decodedKey = Base64.getDecoder().decode(encodedForecastKey);
+        token = new String(decodedKey);
+    }
 
     @Inject
     private CommandListener mentionListener;
--- src/main/java/com/discordee/web/HealthCheckService.java
+++ src/main/java/com/discordee/web/HealthCheckService.java
@@ -1,20 +1,19 @@
 package com.discordee.web;
 
-import org.eclipse.microprofile.health.Health;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
 
 import javax.enterprise.context.ApplicationScoped;
 
-@Health
+@Liveness
 @ApplicationScoped
 public class HealthCheckService implements HealthCheck {
     @Override
     public HealthCheckResponse call() {
         return HealthCheckResponse.named("health")
                 .up()
-                .withData("Author", "Ivo Woltring")
-                .withData("Website", "https://www.ivonet.nl")
+                .withData("Status", "ok")
                 .build();
     }
 }
